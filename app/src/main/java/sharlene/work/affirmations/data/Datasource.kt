983171e580354e09263f6c0eb04d8cf9b9080b21
Crackmindedsk <shakumbhar@gmail.com>
2021-05-30 21:57:55 +0530

    Recycler View initial

---
+++ app/src/main/java/sharlene/work/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package sharlene.work.affirmations.data
+
+import sharlene.work.affirmations.R
+import sharlene.work.affirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations():List<Affirmation>{
+        return listOf(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+
+    }
+}
---
+++ app/src/main/java/sharlene/work/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package sharlene.work.affirmations.model
+
+data class Affirmation (val stringResourceId:Int)
