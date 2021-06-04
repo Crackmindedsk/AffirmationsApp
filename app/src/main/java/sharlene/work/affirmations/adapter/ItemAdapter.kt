53e84d3768ecb0d6404e35272498972c316caab9
Crackmindedsk <shakumbhar@gmail.com>
2021-06-04 16:19:40 +0530

    Recycler View created

--- app/src/main/java/sharlene/work/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/sharlene/work/affirmations/adapter/ItemAdapter.kt
@@ -1,6 +1,7 @@
 package sharlene.work.affirmations.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -8,21 +9,41 @@ import androidx.recyclerview.widget.RecyclerView
 import sharlene.work.affirmations.R
 import sharlene.work.affirmations.model.Affirmation
 
+/**
+ * Adapter for the [RecyclerView] in [MainActivity]. Display [Affirmation] data object.
+ */
 class ItemAdapter(
     private val context: Context,
     private val dataset:List<Affirmation>)
     :RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+
+    //Provide a reference to the views for each data item
+    //Complex data items may need more than one view per item, and
+    //you provide access to all the views for a dat item in a view holder.
+    //Each data item is just an affirmation object.
     class ItemViewHolder(private val view:View):RecyclerView.ViewHolder(view){
         val textView:TextView=view.findViewById(R.id.item_title)
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        
+        //create a new view
+        val adapterLayout=LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        return ItemViewHolder(adapterLayout)
     }
 
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item=dataset[position]
+        holder.textView.text=context.resources.getString(item.stringResourceId)
     }
 
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
     override fun getItemCount()=dataset.size
 }
