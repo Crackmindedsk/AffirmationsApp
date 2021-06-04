f9307a8254491083197f073de22d1e2de0abc8b5
Crackmindedsk <shakumbhar@gmail.com>
2021-06-04 16:35:11 +0530

    Images added to recycler view

--- app/src/main/java/sharlene/work/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/sharlene/work/affirmations/adapter/ItemAdapter.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import sharlene.work.affirmations.R
 import sharlene.work.affirmations.model.Affirmation
 
 /**
- * Adapter for the [RecyclerView] in [MainActivity]. Display [Affirmation] data object.
+ * Adapter for the [RecyclerView] in MainActivity. Display [Affirmation] data object.
  */
 class ItemAdapter(
     private val context: Context,
@@ -21,8 +22,9 @@ class ItemAdapter(
     //Complex data items may need more than one view per item, and
     //you provide access to all the views for a dat item in a view holder.
     //Each data item is just an affirmation object.
-    class ItemViewHolder(private val view:View):RecyclerView.ViewHolder(view){
+    class ItemViewHolder(view:View):RecyclerView.ViewHolder(view){
         val textView:TextView=view.findViewById(R.id.item_title)
+        val imageView:ImageView=view.findViewById(R.id.item_image)
     }
 
     /**
@@ -40,6 +42,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item=dataset[position]
         holder.textView.text=context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
